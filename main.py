d3e8ddad54a525a6bfd8f96e00a81d326cf882d4
boringbug <paradiznik5@gmail.com>
2026-05-10 18:47:27 +0200

    Adjusted the values to find more craters on the moon. Actually found some :)

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from skimage import filters, feature
 from skimage.transform import hough_circle, hough_circle_peaks
 from skimage.draw import circle_perimeter
 
+from time import time_ns
+
 from celestial_objects import Moon
 
 MOON_FILE_PATH = ("LUNA-C-1, 2025-01-11, 1x5L, SkyWatcher, (C), "
@@ -26,16 +28,18 @@ def main() -> None:
     plt.imshow(sobel, origin='lower')
     plt.show()
 
-    print("Starting the hough_circle")
+    print("Starting the hough_circle ...")
 
-    hough_radii = np.arange(50, 110, 1)
+    start_time = time_ns()
+
+    hough_radii = np.arange(10, 110, 1)
     hough_res = hough_circle(edges, hough_radii)
 
     print("Finn hough_circle")
 
     accums, cx, cy, radii = hough_circle_peaks(hough_res,
                                                hough_radii,
-                                               total_num_peaks=20)
+                                               total_num_peaks=100)
     print("Starting the loop")
     for center_y, center_x, radius in zip(cy, cx, radii):
         circy, circx = circle_perimeter(center_y,
@@ -43,8 +47,13 @@ def main() -> None:
                                         radius,
                                         shape=moon.image.shape)
         plt.plot(circx, circy)
+
     moon.plot()
     plt.show()
+
+    end_time = time_ns()
+
+    print(f"Elapsed time was: {(end_time - start_time)*1e-9}s")
 
 
 if __name__ == "__main__":
